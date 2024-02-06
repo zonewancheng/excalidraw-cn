727a2a49dd1b9c65e38c9c4dd2e26469752e5b68
zone <1224972164@qq.com>
2024-02-06 11:39:48 +0800

    feat: remove tooltip

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -25,7 +25,7 @@ const Footer = ({
   showExitZenModeBtn: boolean;
   renderWelcomeScreen: boolean;
 }) => {
-  const { welcomeScreenHelpHintTunnel } = useTunnels();
+  const { footerCenterTunnel, welcomeScreenHelpHintTunnel } = useTunnels();
 
   const device = useDevice();
   const showFinalize =
@@ -70,6 +70,7 @@ const Footer = ({
           </Section>
         </Stack.Col>
       </div>
+      <footerCenterTunnel.Out />
       <div
         className={clsx("layer-ui__wrapper__footer-right zen-mode-transition", {
           "transition-right disable-pointerEvents": appState.zenModeEnabled,
--- src/excalidraw-app/components/EncryptedIcon.tsx
+++ src/excalidraw-app/components/EncryptedIcon.tsx
@@ -5,13 +5,11 @@ import { t } from "../../i18n";
 export const EncryptedIcon = () => (
   <a
     className="encrypted-icon tooltip"
-    href="https://blog.excalidraw.com/end-to-end-encryption/"
     target="_blank"
     rel="noopener noreferrer"
-    aria-label={t("encrypted.link")}
   >
-    <Tooltip label={t("encrypted.tooltip")} long={true}>
+    {/*<Tooltip label={t("encrypted.tooltip")} long={true}>*/}
       {shield}
-    </Tooltip>
+    {/*</Tooltip>*/}
   </a>
 );
