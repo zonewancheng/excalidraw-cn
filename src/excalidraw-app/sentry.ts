02f49336beaaa5f9e9017b5b515779c912c62c20
zone <oospace@foxmail.com>
2024-02-06 10:13:33 +0800

    Update sentry.ts

--- src/excalidraw-app/sentry.ts
+++ src/excalidraw-app/sentry.ts
@@ -16,24 +16,24 @@ const onlineEnv =
     (item) => window.location.hostname.indexOf(item) >= 0,
   );
 
-Sentry.init({
-  dsn: onlineEnv
-    ? "https://7bfc596a5bf945eda6b660d3015a5460@sentry.io/5179260"
-    : undefined,
-  environment: onlineEnv ? SentryEnvHostnameMap[onlineEnv] : undefined,
-  release: process.env.REACT_APP_GIT_SHA,
-  ignoreErrors: [
-    "undefined is not an object (evaluating 'window.__pad.performLoop')", // Only happens on Safari, but spams our servers. Doesn't break anything
-  ],
-  integrations: [
-    new SentryIntegrations.CaptureConsole({
-      levels: ["error"],
-    }),
-  ],
-  beforeSend(event) {
-    if (event.request?.url) {
-      event.request.url = event.request.url.replace(/#.*$/, "");
-    }
-    return event;
-  },
-});
+// Sentry.init({
+//   dsn: onlineEnv
+//     ? "https://7bfc596a5bf945eda6b660d3015a5460@sentry.io/5179260"
+//     : undefined,
+//   environment: onlineEnv ? SentryEnvHostnameMap[onlineEnv] : undefined,
+//   release: process.env.REACT_APP_GIT_SHA,
+//   ignoreErrors: [
+//     "undefined is not an object (evaluating 'window.__pad.performLoop')", // Only happens on Safari, but spams our servers. Doesn't break anything
+//   ],
+//   integrations: [
+//     new SentryIntegrations.CaptureConsole({
+//       levels: ["error"],
+//     }),
+//   ],
+//   beforeSend(event) {
+//     if (event.request?.url) {
+//       event.request.url = event.request.url.replace(/#.*$/, "");
+//     }
+//     return event;
+//   },
+// });
